dc7afdf079e2e4fe75e380de8feac80178965a32
jedraynes <jedr125@gmail.com>
2021-01-28 19:19:13 -0600

    update submissions

--- run_analysis.R
+++ run_analysis.R
@@ -18,42 +18,37 @@ features <- read.table(".\\UCI HAR Dataset\\features.txt", col.names = c("index"
 activity_labels <- read.table(".\\UCI HAR Dataset\\activity_labels.txt", col.names = c("activity_number", "activity_label"))
 
 # testing sets
-subject_test <- read.table(".\\UCI HAR Dataset\\test\\subject_test.txt")
+subject_test <- read.table(".\\UCI HAR Dataset\\test\\subject_test.txt", col.names = c("subject"))
 X_test <- read.table(".\\UCI HAR Dataset\\test\\X_test.txt", col.names = features$feature)
 y_test <- read.table(".\\UCI HAR Dataset\\test\\y_test.txt", col.names = c("activity_number"))
 
 # training sets
-subject_train <- read.table(".\\UCI HAR Dataset\\train\\subject_train.txt")
+subject_train <- read.table(".\\UCI HAR Dataset\\train\\subject_train.txt", col.names = c("subject"))
 X_train <- read.table(".\\UCI HAR Dataset\\train\\X_train.txt", col.names = features$feature)
 y_train <- read.table(".\\UCI HAR Dataset\\train\\y_train.txt", col.names = c("activity_number"))
 
 # merge the test sets
 test_set <- cbind(X_test, y_test)
+test_set <- cbind(subject_test, test_set)
 
 # merge the training sets
 train_set <- cbind(X_train, y_train)
+train_set <- cbind(subject_train, train_set)
 
 # merge the training set and the testing set into one dataset
 df_dirty <- rbind(test_set, train_set)
+
 
 # label the activities
 df_clean <- left_join(df_dirty, activity_labels, by = "activity_number")
 
 # create a second independent tidy dataset with the means
-# we have to use the df_dirty dataset here as the df_clean dataset brought in labels which aren't numeric
-
-# code to get it in table format if desired
-# df_mean <- table(lapply(df_dirty, mean))
-
-df_mean <- colMeans(df_dirty)
+df_mean <- df_clean %>% group_by(df_clean$subject, df_clean$activity_label) %>%
+  summarize_all(.funs = mean)
 
 # write the second dataset for submission
 write.table(df_mean, "step5.txt",row.names = FALSE)
 
 # now we have our two final datasets that are commented out below
-
-# df_clean
-# df_clean is the cleaned dataset that is the merge between the training and testing data that includes the feature labels as well as the categorical activity lable
-
-# df_mean
-# df_mean is the second dataset that has the means of each feature (excl. the categorical activity)
+# View(df_clean)
+# View(df_mean)
